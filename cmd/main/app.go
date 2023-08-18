765b24a033006c9294e8332bf4838ca5e5e15d11
Grandpa Trula <grandpat@un-d6.kzn.21-school.ru>
2023-08-18 17:13:50 +0300

    /:name

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net"
+	"net/http"
+	"time"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request, param httprouter.Params) {
+	name := param.ByName("name")
+	w.Write([]byte(fmt.Sprintf("hello %s", name)))
+}
+
 func main() {
-	router := httprouter.new
+	fmt.Printf("localHost:8080")
+	router := httprouter.New()
+	router.GET("/:name", IndexHandler)
+	listen, err := net.Listen("tcp", "127.0.0.1:8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	server := &http.Server{
+		Handler:      router,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	server.Serve(listen)
 }
