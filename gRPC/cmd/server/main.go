d5a408c9266a7e85a8d751bab7bf8df48875b067
Grandpa Trula <grandpat@si-d5.kzn.21-school.ru>
2023-11-02 22:32:01 +0300

    postgresql

--- gRPC/cmd/server/main.go
+++ gRPC/cmd/server/main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"context"
 	proto "gRPC/proto"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 )
 
-type server struct {
+type GRPCServer struct {
+	proto.UnimplementedAdderServer
+}
+
+func (s *GRPCServer) Add(ctx context.Context, r *proto.AddRequest) (*proto.AddResponse, error) {
+	return &proto.AddResponse{Result: r.GetX() + r.GetY()}, nil
 }
 
 func main() {
 	s := grpc.NewServer()
-	srv := &proto.GRPCServer{} // реализованный интерфейс
+	srv := &GRPCServer{} // реализованный интерфейс
 	proto.RegisterAdderServer(s, srv)
 	listen, err := net.Listen("tcp", ":8080")
 	if err != nil {
--- gRPC/proto/grpcserver.go
+++ gRPC/proto/grpcserver.go
@@ -1,14 +1,2 @@
-package adder
+package proto
 
-import (
-	"context"
-	proto "gRPC/proto"
-)
-
-type GRPCServer struct {
-}
-
-func (s *GRPCServer) Add(ctx context.Context, r *proto.AddRequest) (*proto.AddResponse, error) {
-	return &proto.AddResponse{Result: r.GetX() + r.GetY()}, nil
-
-}
