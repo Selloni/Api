0a77f4cb5a3679e454ccefe27560c509bbc8f832
Grandpa Trula <grandpat@si-d5.kzn.21-school.ru>
2023-11-11 20:46:01 +0300

    using evans

--- gRPC2/main.go
+++ gRPC2/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"math/rand"
 	"net"
+	"strconv"
 	"sync"
 	session "testgRPC/invoicer"
 )
@@ -27,11 +28,14 @@ func NewSessionManager() *SessionManager {
 
 func (s SessionManager) Create(ctx context.Context, in *session.Session) (*session.SessionId, error) {
 	fmt.Println("call Create", in)
-	id := &session.SessionId{ID: string(rand.Intn(99-1) + 1)}
+	id := strconv.Itoa(rand.Intn(98) + 1)
+	fmt.Printf("create ID - %s\n", id)
+	idS := &session.SessionId{ID: id}
+	fmt.Println()
 	s.mu.Lock()
-	s.sessions[id.ID] = in
+	s.sessions[id] = in
 	s.mu.Unlock()
-	return id, nil
+	return idS, nil
 
 }
 func (s SessionManager) Check(ctx context.Context, in *session.SessionId) (*session.Session, error) {
@@ -51,12 +55,13 @@ func (s SessionManager) Delete(ctx context.Context, in *session.SessionId) (*ses
 }
 
 func main() {
-	listen, err := net.Listen("tcp", ":8089")
+
+	listen, err := net.Listen("tcp", ":8081")
 	if err != nil {
 		log.Fatalf("cannot crate listener: %v", err)
 	}
 	MyServer := grpc.NewServer()
-	service := &SessionManager{}
+	service := NewSessionManager()
 	session.RegisterAuthCheckerServer(MyServer, service)
 	fmt.Println("starting server at:8081")
 	MyServer.Serve(listen)
