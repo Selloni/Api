daee46d23e62a4e9e8f949d766df75ea913ddc35
Grandpa Trula <grandpat@si-d5.kzn.21-school.ru>
2023-11-13 11:39:48 +0300

    начал разбиратся в стрименге

---
+++ gRPC_Stream/cmd/client.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"log"
+)
+
+func main() {
+
+	grpcConn, err := grpc.Dial(
+		"127.0.0.1:8081",
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		log.Fatal("cant connect to grpc")
+	}
+	defer grpcConn.Close()
+
+}
---
+++ gRPC_Stream/pkg/translit.go
@@ -0,0 +1,10 @@
+package pkg
+
+type TrServer struct {
+}
+
+func (s TrServer) EmRu(inStream string) error {
+	for {
+		inWord, err := inStream
+	}
+}
